959b1c456b5f4393470bbddb29c957db7c0e8b80
AbolfazlBitaraf <bitaraf1386@gmail.com>
2026-07-08 14:07:15 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-print("hello")
-x = "Abolfzl_BT"
-print(len(x))
-print(x)
+full_name = 'Abolfazl_Bitaraf'
+age = '19'  # or 19
+sport = "Natural Bodybuilding"
+major = 'Computer Engineering'
+interest = 'Mashine learning'
+print (f'''
+Hello evreyone.
+I'm {full_name} and I'm {age} years old.
+My sport is {sport} and my field of study is {major}
+I'm also interested in {interest}
+''')
